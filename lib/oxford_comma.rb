8279d15f5d5a898e61865ab22096c4afdaac0a99
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-11-20 19:08:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,16 @@
 def oxford_comma(array)
-array.join
-end
+  if array.length ==1  
+    array.join
+  elsif array.length ==2 
+    array.join(" and ")
+  end 
+  
 
-def 
+  
+end 
+
+
+def oxford_comma(array)
+  array.join(","+" and ")
+end 
+
